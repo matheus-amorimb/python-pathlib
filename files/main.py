cd2742834e88633317f40eaa6452bc00cd91b0a9
Matheus Batista <mbatista.sarti@gmail.com>
2023-11-19 22:50:23 -0300

    first commit

--- files/main.py
+++ files/main.py
@@ -1,15 +1,25 @@
 from pathlib import Path
 
 path_atual = Path() #retorna o caminho relativo
-files = path_atual / 'files'
+files = path_atual / 'files' #concatena caminhos
 
-absoluto = files.absolute()
+absoluto = files.absolute() #retorna o caminho absoluto
 
 print(absoluto)
 
 p =  Path('teste.txt')
 
-p.touch()
+p.touch() #cria um arquivo
+
+p.unlink() #remove um arquivo
 
 
+path = Path("files/pasta_0/")
+print(list(path.parents))
+
+print(path.parts)
+
+print(path.absolute().parent)
+
+print(list(path.absolute().parents))
 
